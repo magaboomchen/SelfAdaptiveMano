e9b9ef12f3153796c074054e86ca6e134c30cc08
Mike Chen <magaboomchen@163.com>
2020-11-28 20:43:10 +0800

    add a test to find the max SFC length the controller/bess can process(failed! vnfController needs add numa info supports);

---
+++ sam/serverController/vnfController/test/test_MAXLENSFCI.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import logging
+from scapy.all import *
+import time
+
+import pytest
+
+from sam.base import server
+from sam.base.sfc import *
+from sam.base.vnf import *
+from sam.base.server import *
+from sam.base.acl import *
+from sam.base.lb import *
+from sam.base.vpn import *
+from sam.base.nat import *
+from sam.base.command import *
+from sam.base.socketConverter import *
+from sam.base.shellProcessor import ShellProcessor
+from sam.test.fixtures.mediatorStub import *
+from sam.test.fixtures.vnfControllerStub import *
+from sam.test.testBase import *
+from sam.serverController.classifierController import *
+
+MANUAL_TEST = True
+TESTER_SERVER_DATAPATH_IP = "2.2.0.36"
+TESTER_SERVER_DATAPATH_MAC = "f4:e9:d4:a3:53:a0"
+
+SFF0_DATAPATH_IP = "2.2.0.38"
+SFF0_DATAPATH_MAC = "00:1b:21:c0:8f:98"
+SFF0_CONTROLNIC_IP = "192.168.0.173"
+SFF0_CONTROLNIC_MAC = "18:66:da:85:1c:c3"
+
+MAX_SFCI_NUM = 1
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("pika").setLevel(logging.WARNING)
+
+class TestVNFSFCIAdderClass(TestBase):
+    @pytest.fixture(scope="function")
+    def setup_addSFCI(self):
+        # setup
+        self.sP = ShellProcessor()
+        self.clearQueue()
+
+        classifier = self.genClassifier(datapathIfIP = CLASSIFIER_DATAPATH_IP)
+        self.mediator = MediatorStub()
+        self.server = self.genTesterServer(TESTER_SERVER_DATAPATH_IP,
+            TESTER_SERVER_DATAPATH_MAC)
+
+        self.runSFFController()
+        self.runVNFController()
+
+        self.sfc = self.genBiDirectionSFC(classifier)
+
+        self.genSFCIList()
+        self.genAddSFCICmdList()
+
+        yield
+        # teardown
+        self.delVNFI4Server()
+        self.killSFFController()
+        self.killVNFController()
+
+    def genSFCIList(self):
+        self.sfciList = []
+        for sfciIndex in range(MAX_SFCI_NUM):
+            self.sfci = self.genBiDirection10BackupSFCI()
+            self.sfciList.append(self.sfci)
+
+    def genAddSFCICmdList(self):
+        self.addSFCICmdList = []
+        for sfciIndex in range(MAX_SFCI_NUM):
+            sfci = self.sfciList[sfciIndex]
+            addSFCICmd = self.mediator.genCMDAddSFCI(self.sfc, sfci)
+            logging.info("sfci id: {0}".format(sfci.SFCIID))
+            self.addSFCICmdList.append(addSFCICmd)
+
+    def gen10BackupVNFISequence(self, SFCLength=7):
+        # hard-code function
+        logging.info("use override function")
+        VNFISequence = []
+
+        server = Server("ens3", SFF0_DATAPATH_IP, SERVER_TYPE_NORMAL)
+        server.setServerID(SERVERID_OFFSET + 1)
+        server.setControlNICIP(SFF0_CONTROLNIC_IP)
+        server.setControlNICMAC(SFF0_CONTROLNIC_MAC)
+        server.setDataPathNICMAC(SFF0_DATAPATH_MAC)
+        server.updateResource()
+
+        vnfi = VNFI(VNF_TYPE_FORWARD, VNFType=VNF_TYPE_FORWARD, 
+            VNFIID=uuid.uuid1(), node=server)
+        vnfi.maxCPUNum = 1
+        index = 0
+        VNFISequence.append([])
+        VNFISequence[index].append(vnfi)
+
+        config = {}
+        config['ACL'] = self.genTestFWRules()
+        vnfi = VNFI(VNF_TYPE_FW, VNFType=VNF_TYPE_FW, 
+            VNFIID=uuid.uuid1(), config=config, node=server)
+        vnfi.maxCPUNum = 1
+        index = 1
+        VNFISequence.append([])
+        VNFISequence[index].append(vnfi)
+
+        # vnfi = VNFI(VNF_TYPE_IDS, VNFType=VNF_TYPE_IDS, 
+        #     VNFIID=uuid.uuid1(), node=server)
+        # vnfi.maxCPUNum = 1
+        # index = 2
+        # VNFISequence.append([])
+        # VNFISequence[index].append(vnfi)
+
+        # vnfi = VNFI(VNF_TYPE_MONITOR, VNFType=VNF_TYPE_MONITOR, 
+        #     VNFIID=uuid.uuid1(), node=server)
+        # vnfi.maxCPUNum = 1
+        # index = 3
+        # VNFISequence.append([])
+        # VNFISequence[index].append(vnfi)
+
+        # vnfi = VNFI(VNF_TYPE_LB, VNFType=VNF_TYPE_LB, 
+        #     VNFIID=uuid.uuid1(), node=server)
+        # vnfi.maxCPUNum = 1
+        # index = 4
+        # VNFISequence.append([])
+        # VNFISequence[index].append(vnfi)
+
+        # vnfi = VNFI(VNF_TYPE_NAT, VNFType=VNF_TYPE_NAT, 
+        #     VNFIID=uuid.uuid1(), node=server)
+        # vnfi.maxCPUNum = 1
+        # index = 5
+        # VNFISequence.append([])
+        # VNFISequence[index].append(vnfi)
+
+        # vnfi = VNFI(VNF_TYPE_VPN, VNFType=VNF_TYPE_VPN, 
+        #     VNFIID=uuid.uuid1(), node=server)
+        # vnfi.maxCPUNum = 1
+        # index = 6
+        # VNFISequence.append([])
+        # VNFISequence[index].append(vnfi)
+
+        logging.info("VNFISequence: {0}".format(VNFISequence))
+        return VNFISequence
+
+    def genTestFWRules(self):
+        rules = []
+        rules.append(ACLTuple(ACL_ACTION_ALLOW, proto=ACL_PROTO_TCP, srcAddr=OUTTER_CLIENT_IP, dstAddr=WEBSITE_REAL_IP, 
+            srcPort=(1234, 1234), dstPort=(80, 80)))
+        rules.append(ACLTuple(ACL_ACTION_ALLOW, proto=ACL_PROTO_TCP, srcAddr=WEBSITE_REAL_IP, dstAddr=OUTTER_CLIENT_IP,
+            srcPort=(80, 80), dstPort=(1234, 1234)))
+        rules.append(ACLTuple(ACL_ACTION_DENY))
+        return rules
+
+    def test_addSFCI(self, setup_addSFCI):
+        # exercise
+        logging.info("exercise")
+        self.addSFCI2SFF()
+        self.addVNFI2Server()
+
+        # verifiy
+        logging.info("please start performance profiling" \
+            "after profiling, press any key to quit.")
+        raw_input()
+
+    def addSFCI2SFF(self):
+        logging.info("setup add SFCI to sff")
+        for sfciIndex in range(MAX_SFCI_NUM):
+            addSFCICmd = self.addSFCICmdList[sfciIndex]
+            addSFCICmd.cmdID = uuid.uuid1()
+            self.sendCmd(SFF_CONTROLLER_QUEUE,
+                MSG_TYPE_SSF_CONTROLLER_CMD , addSFCICmd)
+            cmdRply = self.recvCmdRply(MEDIATOR_QUEUE)
+            assert cmdRply.cmdID == addSFCICmd.cmdID
+            assert cmdRply.cmdState == CMD_STATE_SUCCESSFUL
+
+    def addVNFI2Server(self):
+        for sfciIndex in range(MAX_SFCI_NUM):
+            # logging.info("press any key to continue place vnfi")
+            # raw_input()
+            addSFCICmd = self.addSFCICmdList[sfciIndex]
+            addSFCICmd.cmdID = uuid.uuid1()
+            self.sendCmd(VNF_CONTROLLER_QUEUE,
+                MSG_TYPE_VNF_CONTROLLER_CMD , addSFCICmd)
+            cmdRply = self.recvCmdRply(MEDIATOR_QUEUE)
+            assert cmdRply.cmdID == addSFCICmd.cmdID
+            assert cmdRply.cmdState == CMD_STATE_SUCCESSFUL
+
+    def delVNFI4Server(self):
+        logging.warning("Deleting VNFI")
+        for sfciIndex in range(MAX_SFCI_NUM):
+            sfci = self.sfciList[sfciIndex]
+            delSFCICmd = self.mediator.genCMDDelSFCI(self.sfc, sfci)
+            self.sendCmd(VNF_CONTROLLER_QUEUE, MSG_TYPE_VNF_CONTROLLER_CMD, delSFCICmd)
+            cmdRply = self.recvCmdRply(MEDIATOR_QUEUE)
+            assert cmdRply.cmdID == delSFCICmd.cmdID
+            assert cmdRply.cmdState == CMD_STATE_SUCCESSFUL
