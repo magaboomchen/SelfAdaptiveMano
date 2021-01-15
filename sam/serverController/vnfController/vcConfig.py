312968df25abab1b53c22c9711997e14cf3a63be
tymss <1042318957@qq.com>
2021-01-15 20:40:49 +0800

    fix a bug for monitor

--- sam/serverController/vnfController/vcConfig.py
+++ sam/serverController/vnfController/vcConfig.py
@@ -7,7 +7,7 @@ class VCConfig:
     
     DOCKER_TCP_PORT = 5982  # maybe unsafe
 
-    DPDKINFO_BUF = 65536  # used for element DPDKInfo
+    DPDKINFO_BUF = 65535  # used for element DPDKInfo
 
     DEBUG = True  # if you set debug=True, the container will not be removed even if the app is terminated.
                   # !!!please run docker rm XXX to free resources of the container.!!!
@@ -20,30 +20,30 @@ class VCConfig:
     FWD_IMAGE_DPDK = 'dpdk-app-testpmd'
     FWD_APP_DPDK = './x86_64-native-linuxapp-gcc/app/testpmd'
 
-    FWD_IMAGE_CLICK = 'release/fastclick-vnf'
+    FWD_IMAGE_CLICK = 'fastclick-master'
     #FWD_IMAGE_CLICK = 'fastclick-vnf/disable-batch' 
     FWD_APP_CLICK = './click-conf/fwd.click'
 
-    FW_IMAGE_CLICK = 'release/fastclick-vnf'
+    FW_IMAGE_CLICK = 'fastclick-master'
     #FW_IMAGE_CLICK = 'fastclick-vnf/disable-batch'
     FW_APP_CLICK = './click-conf/statelessFW.click'
     FW_RULE_DIR = '/rule'
     FW_RULE_PATH = '/rule/statelessFW'
 
-    LB_IMAGE_CLICK = 'release/fastclick-vnf'
+    LB_IMAGE_CLICK = 'fastclick-master'
     #LB_IMAGE_CLICK = 'fastclick-vnf'
     LB_APP_CLICK = './click-conf/lb.click'
 
-    MON_IMAGE_CLICK = 'release/fastclick-vnf'
+    MON_IMAGE_CLICK = 'fastclick-master'
     #MON_IMAGE_CLICK = 'fastclick-vnf/disable-batch'
     MON_APP_CLICK = './click-conf/monitor.click'
     MON_TCP_PORT = 8888  # maybe unsafe
 
-    NAT_IMAGE_CLICK = 'release/fastclick-vnf'
+    NAT_IMAGE_CLICK = 'fastclick-master'
     #NAT_IMAGE_CLICK = 'fastclick-vnf/disable-batch'
     NAT_APP_CLICK = './click-conf/nat.click'
 
-    VPN_IMAGE_CLICK = 'release/fastclick-vnf'
+    VPN_IMAGE_CLICK = 'fastclick-master'
     #VPN_IMAGE_CLICK = 'fastclick-vnf/disable-batch'
     VPN_APP_CLICK = './click-conf/vpn.click'
 
