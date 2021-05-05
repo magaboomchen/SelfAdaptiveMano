1a38857cf5f3e8e4c547e535929f1526dcef0c72
THlym <37110656+THlym@users.noreply.github.com>
2021-05-05 16:27:05 +0800

    simulator: handle commands from mq & console

--- sam/simulator/simulator.py
+++ sam/simulator/simulator.py
@@ -17,10 +17,17 @@ from sam.base.shellProcessor import ShellProcessor
 from sam.base.loggerConfigurator import LoggerConfigurator
 from sam.base.exceptionProcessor import ExceptionProcessor
 from sam.simulator.simulatorInfoBaseMaintainer import SimulatorInfoBaseMaintainer
+from sam.base.routingMorphic import *
+from sam.base.path import *
+from Queue import Queue, Empty
+from getopt import getopt
+import random
+import threading
+import os
 
 
 class Simulator(object):
-    def __init__(self):
+    def __init__(self, op_input):
         logConfigur = LoggerConfigurator(__name__, './log',
                             'simulator.log', level='debug')
         self.logger = logConfigur.getLogger()
@@ -39,9 +46,21 @@ class Simulator(object):
         self._messageAgent.setRabbitMqServer("192.168.5.124", "mq", "123456")
         self._messageAgent.startRecvMsg(SIMULATOR_QUEUE)
 
+        self.op_input=op_input
+
     def startSimulator(self):
         try:
             while True:
+                try:
+                    while True:
+                        command=self.op_input.get_nowait()
+                        command=command.strip()
+                        if command:
+                            self._op_input_handler(command)
+                        self.op_input.task_done()
+                except Empty:
+                    pass
+
                 msg = self._messageAgent.getMsg(SIMULATOR_QUEUE)
                 msgType = msg.getMessageType()
                 if msgType == None:
@@ -83,8 +102,7 @@ class Simulator(object):
             ExceptionProcessor(self.logger).logException(ex, "simulator")
             self._cm.changeCmdState(cmd.cmdID, CMD_STATE_FAIL)
         finally:
-            cmdRply = CommandReply(cmd.cmdID, self._cm.getCmdState(cmd.cmdID))
-            cmdRply.attributes["source"] = {"simulator"}
+            cmdRply = CommandReply(cmd.cmdID, self._cm.getCmdState(cmd.cmdID),dict(attributes, source='simulator'))
             rplyMsg = SAMMessage(MSG_TYPE_SIMULATOR_CMD_REPLY, cmdRply)
             self._messageAgent.sendMsg(MEDIATOR_QUEUE, rplyMsg)
 
@@ -94,37 +112,227 @@ class Simulator(object):
 
     def _addSFCIHandler(self, cmd):
         sfc=cmd.attributes['sfc']
+        sfcUUID=sfc.sfcUUID
+        vNFTypeSequence=sfc.vNFTypeSequence
+        maxScalingInstanceNumber=sfc.maxScalingInstanceNumber
+        backupInstanceNumber=sfc.backupInstanceNumber
+        applicationType=sfc.applicationType
+        directions=sfc.directions
+        for direction in directions:
+            ingress=direction['ingress']
+            ingressID=ingress.getServerID()
+            egress=direction['egress']
+            egressID=egress.getServerID()
+            assert ingressID in self._sib.servers and self._sib.servers[ingressID]['server'].getServerType()==SERVER_TYPE_CLASSIFIER
+            assert egressID in self._sib.servers and self._sib.servers[egressID]['server'].getServerType()==SERVER_TYPE_CLASSIFIER
+        slo=sfc.slo
+        latencyBound=slo.latencyBound
+        throughput=slo.throughput
+        routingMorphic=sfc.routingMorphic # may be None
         sfci=cmd.attributes['sfci']
+        sfciID=sfci.sfciID
+        vnfiSequence=sfci.vnfiSequence
+        for vnfiSeqStage in vnfiSequence:
+            assert len(vnfiSeqStage)==1 # no backup
+            vnfi=vnfiSeqStage[0]
+            vnfID=vnfi.vnfID
+            vnfType=vnfi.vnfType
+            vnfiID=vnfi.vnfiID
+            node=vnfi.node
+            if isinstance(node,Server):
+                nodeID=node.getServerID()
+                assert nodeID in self._sib.servers and self._sib.servers[nodeID]['server'].getServerType()==SERVER_TYPE_NFVI
+            elif isinstance(node,Switch):
+                nodeID=node.switchID
+                assert node.programmable
+                assert nodeID in self._sib.switches
+            else:
+                raise ValueError('vnfi node is neither server nor switch')
+        forwardingPathSet=sfci.forwardingPathSet
+        mappingType=forwardingPathSet.mappingType
+        primaryForwardingPath=forwardingPathSet.primaryForwardingPath
+        assert len(primaryForwardingPath)==len(directions)
+        for direction in directions:
+            dirID=direction['ID']
+            if dirID==0:
+                pathlist=primaryForwardingPath[DIRECTION1_PATHID_OFFSET]
+            elif dirID==1:
+                pathlist=primaryForwardingPath[DIRECTION2_PATHID_OFFSET]
+            assert len(pathlist)==len(vnfiSequence)+1
+            ingress=direction['ingress']
+            ingressID=ingress.getServerID()
+            egress=direction['egress']
+            egressID=egress.getServerID()
+            assert ingressID==pathlist[0][0][1]
+            assert (pathlist[0][0][1],pathlist[0][1][1]) in self._sib.serverLinks
+            assert egressID==pathlist[-1][-1][1]
+            assert (pathlist[-1][-2][1],pathlist[-1][-1][1]) in self._sib.serverLinks
+            for i,vnfiSeqStage in enumerate(vnfiSequence):
+                vnfi=vnfiSeqStage[0] # no backup
+                node=vnfi.node
+                if isinstance(node,Server):
+                    nodeID=node.getServerID()
+                elif isinstance(node,Switch):
+                    nodeID=node.switchID
+                assert pathlist[i][-1][1]==nodeID
+                assert pathlist[i+1][0][1]==nodeID
+                if isinstance(node,Server):
+                    assert (pathlist[i][-2][1],pathlist[i][-1][1]) in self._sib.serverLinks
+                    assert (pathlist[i+1][0][1],pathlist[i+1][1][1]) in self._sib.serverLinks
+                elif isinstance(node,Switch):
+                    assert (pathlist[i][-2][1],pathlist[i][-1][1]) in self._sib.links
+                    assert (pathlist[i+1][0][1],pathlist[i+1][1][1]) in self._sib.links
+            for path in pathlist:
+                for _,switchID in path[1:-1]:
+                    assert switchID in self._sib.switches
+                for i in range(1, len(path)-2):
+                    assert (path[i][1],path[i+1][1]) in self._sib.links
+
         # TODO
+        # check resources
+        # assign resources
+        # store information
+        self._sib.sfcs[sfcUUID]={'sfc':sfc}
+        self._sib.sfcis[sfciID]={'sfc':sfc, 'sfci':sfci, 'traffics':set()}
 
     def _delSFCIHandler(self, cmd):
         sfc=cmd.attributes['sfc']
         sfci=cmd.attributes['sfci']
         # TODO
+        # release resources
+        # purge information
+        for trafficID in self._sib.sfcis[sfciID]['traffics']:
+            self._sib.flows.pop(trafficID)
+        self._sib.sfcis.pop(sfciID)
 
     def _delSFCHandler(self, cmd):
         pass
         # TODO
 
     def _getServerSetHandler(self, cmd):
-        {serverID:{"server":server, "Active": isActive, "timestamp":datetime.datetime.now()}}
-        return {}
-        # TODO
+        # format ref: SeverManager.serverSet, SeverManager._storeServerInfo
+        result={}
+        for serverID, serverInfo in self._sib.servers.items():
+            result[serverID]={'server':serverInfo['server'], 'Active':serverInfo['Active'], 'timestamp':serverInfo['timestamp']}
+        return {'servers': result}
 
     def _getTopologyHandler(self, cmd):
         # see ryu/topoCollector.py -> TopoCollector.get_topology_handler
-        return {}
-        # TODO
+        switches={}
+        for switchID, switchInfo in self._sib.switches.items():
+            switches[switchID]={'switch': switchInfo['switch'], 'Active':switchInfo['Active']}
+        links={}
+        for (srcNodeID, dstNodeID), linkInfo in self._sib.links.items():
+            links[(srcNodeID, dstNodeID)]={'link': linkInfo['link'], 'Active': linkInfo['Active']}
+        return {'switches': switches, 'links': links}
 
     # def _getSFCIStateHandler(self, cmd):
     #     pass
     #     # TODO
 
     def _getFlowSetHandler(self, cmd):
-        return {}
-        # TODO
+        result={}
+        return {'flows': result}
 
+    def _op_input_handler(self, cmdstr):
+        self.logger.debug('Simulator received operator input: '+cmdstr)
+        try:
+            cmdtype=cmdstr.split(' ',1)[0].lower()
+            if cmdtype=='reset':
+                opt, arg = getopt(cmdstr.split(' ')[1:],'',())
+                if not opt and not arg: # reset
+                    self._sib.reset()
+                else:
+                    raise ValueError(cmdtype+' '+cmdstr)
+            elif cmdtype=='load':
+                opt, arg = getopt(cmdstr.split(' ')[1:],'',())
+                if not opt and len(arg)==1: # load <filename>
+                    self._sib.loadTopology(arg[0])
+                else:
+                    raise ValueError(cmdtype+' '+cmdstr)
+            elif cmdtype=='server':
+                opt, arg = getopt(cmdstr.split(' ')[1:],'',())
+                if not opt and len(arg)==2 and arg[1] in ('up','down'): # server <serverID> up|down
+                    serverID=int(arg[0])
+                    self._sib.servers[serverID]['Active']=(arg[1].lower()=='up')
+                else:
+                    raise ValueError(cmdtype+' '+cmdstr)
+            elif cmdtype=='switch':
+                opt, arg = getopt(cmdstr.split(' ')[1:],'',())
+                if not opt and len(arg)==2 and arg[1] in ('up','down'): # switch <switchID> up|down
+                    switchID=int(arg[0])
+                    self._sib.switches[switchID]['Active']=(arg[1].lower()=='up')
+                else:
+                    raise ValueError(cmdtype+' '+cmdstr)
+            elif cmdtype=='link':
+                opt, arg = getopt(cmdstr.split(' ')[1:],'',())
+                if not opt and len(arg)==3 and arg[2] in ('up','down'): # link <srcID> <dstID> up|down
+                    srcID=int(arg[0])
+                    dstID=int(arg[1])
+                    self._sib.links[(srcID,dstID)]['Active']=(arg[2].lower()=='up')
+                else:
+                    raise ValueError(cmdtype+' '+cmdstr)
+            elif cmdtype=='traffic':
+                trafficID=cmdstr.split(' ')[1]
+                opt, arg = getopt(cmdstr.split(' ')[2:],'',('trafficPattern=','value=','min=','max='))
+                opt = dict(opt)
+                if not arg and opt['--trafficPattern']=='constant' and '--value' in opt: # traffic <trafficID> --trafficPattern constant --value <value in Mbps>:
+                    self._sib.flows[trafficID]['bw']=lambda :float(opt['--value'])
+                elif not arg and opt['--trafficPattern']=='uniform' and '--min' in opt and '--max' in opt: # traffic <trafficID> --trafficPattern uniform --min <value> --max <value>:
+                    self._sib.flows[trafficID]['bw']=lambda :(float(opt['--min'])+(float(opt['--max'])-float(opt['--min']))*random.random())
+                else:
+                    raise ValueError(cmdtype+' '+cmdstr)
+            else:
+                objtype=cmdstr.split(' ',2)[1].lower()
+                if cmdtype=='add' and objtype=='traffic':
+                    trafficID=cmdstr.split(' ')[2]
+                    sfciID=cmdstr.split(' ')[3]
+                    opt, arg = getopt(cmdstr.split(' ')[4:],'',())
+                    if not arg and 'trafficRate' in opt: # add traffic <trafficID> <sfciID> --trafficRate <value in Mbps>
+                        if trafficID in self._sib.flows:
+                            raise KeyError(trafficID)
+                        self._sib.flows[trafficID]={'bw':(lambda :float(opt['--trafficRate'])), 'sfciID':sfciID}
+                        self._sib.sfcis[sfciID]['traffics'].add(trafficID)
+                elif cmdtype=='del' and objtype=='traffic':
+                    trafficID=cmdstr.split(' ')[2]
+                    opt, arg = getopt(cmdstr.split(' ')[3:],'',())
+                    if not opt and not arg: # del traffic <trafficID>
+                        sfciID=self._sib.flows[trafficID]['sfciID']
+                        self._sib.sfcis[sfciID]['traffics'].remove(trafficID)
+                        self._sib.flows.pop(trafficID)
+            print('OK: '+cmdstr)
+        except Exception as ex:
+            print('FAIL: '+cmdstr)
+            ExceptionProcessor(self.logger).logException(ex, "simulator command")
+
+def get_op_input(op_input):
+    op_input.join()
+    try:
+        while True:
+            time.sleep(0.2)
+            op_input.put(raw_input('> '))
+            op_input.join()
+    except EOFError:
+        pass
 
 if __name__ == "__main__":
-    s = Simulator()
+    op_input=Queue()
+    op_input.put('reset')
+    self_location=os.path.dirname(os.path.abspath(__file__))
+    try:
+        initfile=open(os.path.join(self_location, 'simulator_init_TEST'),'r')
+    except IOError:
+        try:
+            initfile=open(os.path.join(self_location, 'simulator_init'),'r')
+        except IOError:
+            pass
+    try:
+        for line in initfile:
+            op_input.put(line)
+    except NameError:
+        pass
+    thrd=threading.Thread(target=get_op_input, name='simulator input', args=(op_input,))
+    thrd.setDaemon(True)
+    thrd.start()
+    s = Simulator(op_input)
     s.startSimulator()
--- sam/simulator/simulatorInfoBaseMaintainer.py
+++ sam/simulator/simulatorInfoBaseMaintainer.py
@@ -10,41 +10,77 @@ import uuid
 
 from sam.base.pickleIO import PickleIO
 from sam.measurement.dcnInfoBaseMaintainer import DCNInfoBaseMaintainer
+from sam.base.link import Link
+from re import match
+from sam.base.socketConverter import SocketConverter
 
 
 class SimulatorInfoBaseMaintainer(DCNInfoBaseMaintainer):
     def __init__(self):
         super(SimulatorInfoBaseMaintainer, self).__init__()
         self.pIO = PickleIO()
+        self.sc = SocketConverter()
+
         self.links = {}
         self.switches = {}
         self.servers = {}
+        self.serverLinks = {}
+        self.sfcs = {}
+        self.sfcis = {}
+        self.flows = {}
+
+    def reset(self):
+        self.links.clear()
+        self.switches.clear()
+        self.servers.clear()
+        self.serverLinks.clear()
+        self.sfcs.clear()
+        self.sfcis.clear()
+        self.flows.clear()
 
     def loadTopology(self, topoFilePath):
+        self.reset()
         self.topologyDict = self.pIO.readPickleFile(topoFilePath)
         # more details in /sam/simulator/test/readme.md
-        self.links = self.topologyDict["links"]
-        self.switches = self.topologyDict["switches"]
-        self.servers = self.topologyDict["servers"]
 
-    def turnOffSwitch(self, switch):
+        self.links.update(self.topologyDict["links"])
+        self.switches.update(self.topologyDict["switches"])
+        self.servers.update(self.topologyDict["servers"])
+
+        for serverID, serverInfo in self.servers.items():
+            serverInfo['switchID']=[]
+            server=serverInfo['server']
+            DatapathIP=server.getDatapathNICIP()
+            for switchID, switchInfo in self.switches.items():
+                switch=switchInfo['switch']
+                switchNet=switch.lanNet
+                if self.sc.isLANIP(DatapathIP, switchNet):
+                    break
+            else:
+                continue
+            bw=server.getNICBandwidth()
+            self.serverLinks[(serverID, switchID)]={'link':Link(serverID, switchID, bw), 'Active':True, 'Status':None}
+            self.serverLinks[(switchID, serverID)]={'link':Link(switchID, serverID, bw), 'Active':True, 'Status':None}
+            serverInfo['switchID'].append(switchID)
+
+    def turnOffSwitch(self, switchID):
         pass
         # CLI > switch 3 down
 
-    def turnOnSwitch(self, switch):
+    def turnOnSwitch(self, switchID):
         pass
         # CLI > switch 3 up
 
-    def turnOffLink(self, link):
+    def turnOffLink(self, srcID, dstID):
         pass
 
-    def turnOnLink(self, link):
+    def turnOnLink(self, srcID, dstID):
         pass
 
-    def turnOnServer(self, server):
+    def turnOnServer(self, serverID):
         pass
 
-    def turnOffServer(self, server):
+    def turnOffServer(self, serverID):
         pass
 
     def getSFCIFlowIdentifierDict(self, sfci, stageIndex):
