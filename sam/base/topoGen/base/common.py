db72d34112dd9fa564928366a14053ea6653c976
李扬浦 <li-yangpu@qq.com>
2022-05-25 15:43:52 +0800

    Update topology.

--- sam/base/topoGen/base/common.py
+++ sam/base/topoGen/base/common.py
@@ -4,7 +4,7 @@
 # basic settings
 MIN_EXP_NUM = 0     # experiments number, just ignore this
 MAX_EXP_NUM = 10 # please use shell to run multiple exp simultaneously
-VNF_NUM = 16    # VNF type number
+VNF_NUM = 15    # VNF type number
 SFC_REQUEST_NUM = 100   # ignore this
 SERVER_NUM = 20     # server number per ToR switch
 SERVER_CLASSIFIER_NUM = 1   # Default is 1, used as the entrance of each SFC
