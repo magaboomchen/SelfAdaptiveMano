1f9e107747a8c6d48aeaf4d78908630939cd675c
youyl <1078307713@qq.com>
2022-08-08 13:03:58 +0000

    p4-0808

--- sam/switchController/p4Controller/agent/p4Agent.py
+++ sam/switchController/p4Controller/agent/p4Agent.py
@@ -15,7 +15,7 @@ class P4Agent:
                 self.target,
                 [indextable.make_key([
                     client.KeyTuple('hdr.nsh_h.service_path_index', _service_path_index),
-                    client.KeyTuple('hdr.nsh_h.service_index', _service_index)
+                    client.KeyTuple('hdr.nsh_h.service_index', _service_index),
                     client.KeyTuple('ig_md.index_val', i)
                 ])],
                 [indextable.make_data([], 'SwitchIngress.hit_index')]
@@ -26,10 +26,32 @@ class P4Agent:
                 self.target,
                 [monitortable.make_key([
                     client.KeyTuple('hdr.nsh_h.service_path_index', _service_path_index),
-                    client.KeyTuple('hdr.nsh_h.service_index', _service_index)
+                    client.KeyTuple('hdr.nsh_h.service_index', _service_index),
                     client.KeyTuple('ig_md.hash_val', i)
                 ])],
                 [monitortable.make_data([], 'SwitchIngress.hit_monitor')]
+            )
+
+    def removeMonitor(self, _service_path_index, _service_index):
+        indextable = self.bfrtinfo.table_get('SwitchIngress.MonitorIndex')
+        for i in range(0, 256):
+            indextable.entry_del(
+                self.target,
+                [indextable.make_key([
+                    client.KeyTuple('hdr.nsh_h.service_path_index', _service_path_index),
+                    client.KeyTuple('hdr.nsh_h.service_index', _service_index),
+                    client.KeyTuple('ig_md.index_val', i)
+                ])]
+            )
+        monitortable = self.bfrtinfo.table_get('SwitchIngress.FlowMonitor')
+        for i in range(0, 65536):
+            monitortable.entry_del(
+                self.target,
+                [monitortable.make_key([
+                    client.KeyTuple('hdr.nsh_h.service_path_index', _service_path_index),
+                    client.KeyTuple('hdr.nsh_h.service_index', _service_index),
+                    client.KeyTuple('ig_md.hash_val', i)
+                ])]
             )
 
     def addIEGress(self, _service_path_index, _service_index, _outport = 64):
@@ -60,16 +82,14 @@ class P4Agent:
             [ingresstable.make_key([
                 client.KeyTuple('hdr.nsh_h.service_path_index', _service_path_index),
                 client.KeyTuple('hdr.nsh_h.service_index', _service_index)
-            ])],
-            [ingresstable.make_data([], 'SwitchIngress.hit_ingress')]
+            ])]
         )
         egresstable.entry_del(
             self.target,
             [egresstable.make_key([
                 client.KeyTuple('hdr.nsh_h.service_path_index', _service_path_index),
                 client.KeyTuple('hdr.nsh_h.service_index', _service_index)
-            ])],
-            [egresstable.make_data([], 'SwitchIngress.hit_egress')]
+            ])]
         )
 
     def addRateLimiter(self, _service_path_index, _service_index):
@@ -107,8 +127,7 @@ class P4Agent:
             [ratelimiter.make_key([
                 client.KeyTuple('hdr.nsh_h.service_path_index', _service_path_index),
                 client.KeyTuple('hdr.nsh_h.service_index', _service_index)
-            ])],
-            [ratelimiter.make_data([], 'SwitchIngress.hit_ratelimiter')]
+            ])]
         )
 
     def addv4FWentry(self, _service_path_index, _service_index, _src_addr, _dst_addr, _src_mask, _dst_mask, _nxt_hdr, _priority, _is_drop):
@@ -147,8 +166,7 @@ class P4Agent:
                 client.KeyTuple("hdr.ipv4_h.dst_addr", _dst_addr, _dst_mask),
                 client.KeyTuple("hdr.ipv4_h.nxt_hdr", _nxt_hdr, 255),
                 client.KeyTuple("$MATCH_PRIORITY", _priority)
-            ])],
-            [statelessfw.make_data([], actionname)]
+            ])]
         )
 
     def addv6FWentry(self, _service_path_index, _service_index, _src_addr, _dst_addr, _src_mask, _dst_mask, _nxt_hdr, _priority, _is_drop):
@@ -187,8 +205,7 @@ class P4Agent:
                 client.KeyTuple("hdr.ipv6_h.dst_addr", _dst_addr, _dst_mask),
                 client.KeyTuple("hdr.ipv6_h.nxt_hdr", _nxt_hdr, 255),
                 client.KeyTuple("$MATCH_PRIORITY", _priority)
-            ])],
-            [statelessfw.make_data([], actionname)]
+            ])]
         )
 
     def queryIndex(self, _service_path_index, _service_index, _index_val):
--- sam/switchController/p4Controller/p4controller.py
+++ sam/switchController/p4Controller/p4controller.py
@@ -1,14 +1,21 @@
 import sam
 
 from agent.p4Agent import P4Agent
-from agent.turbonetAgent import TurbonetAgent
 
 from sam.base.command import CMD_TYPE_GET_SFCI_STATE, CommandReply, CMD_TYPE_ADD_SFCI, CMD_TYPE_DEL_SFCI, CMD_STATE_SUCCESSFUL, CMD_STATE_FAIL, CMD_STATE_PROCESSING
 from sam.base.server import Server
-from sam.base.messageAgent import SAMMessage, MessageAgent, P4CONTROLLER_QUEUE, MSG_TYPE_P4CONTROLLER_CMD, MSG_TYPE_P4CONTROLLER_CMD_REPLY, MEDIATOR_QUEUE
+from sam.base.switch import Switch
+from sam.base.messageAgent import SAMMessage, MessageAgent, P4CONTROLLER_QUEUE, MSG_TYPE_P4CONTROLLER_CMD, MSG_TYPE_P4CONTROLLER_CMD_REPLY, MEDIATOR_QUEUE, TURBONET_ZONE
 from sam.base.loggerConfigurator import LoggerConfigurator
 from sam.base.exceptionProcessor import ExceptionProcessor
 from sam.base.vnf import VNFIStatus
+from sam.base.routingMorphic import IPV4_ROUTE_PROTOCOL, IPV6_ROUTE_PROTOCOL, ROCEV1_ROUTE_PROTOCOL, SRV6_ROUTE_PROTOCOL
+from sam.base.vnf import VNF_TYPE_FORWARD, VNF_TYPE_FW, VNF_TYPE_MONITOR, VNF_TYPE_LB, VNF_TYPE_NAT, VNF_TYPE_RATELIMITER, VNF_TYPE_VPN
+from sam.base.acl import ACLTable, ACLTuple
+from sam.base.rateLimiter import RateLimiterConfig
+
+P4CONTROLLER_P4_SWITCH_ID_1 = 20
+P4CONTROLLER_P4_SWITCH_ID_2 = 21
 
 class P4Controller:
     def __init__(self, _zonename):
@@ -16,15 +23,18 @@ class P4Controller:
         logConf = LoggerConfigurator(__name__, './log', 'p4Controller.log', level='debug')
         self.logger = logConf.getLogger()
         self.logger.info('Initialize P4 controller.')
-        self.zoneName = _zonename
+        self.zonename = _zonename
         self._messageAgent = MessageAgent()
         self.queueName = self._messageAgent.genQueueName(P4CONTROLLER_QUEUE, _zonename)
         self._messageAgent.startRecvMsg(self.queueName)
         # controller init
         self._commands = {}
+        self._commandresults = {}
         self._sfclist = {}
         self._p4agent = {}
-        self._p4agent[0] = P4Agent('192.168.100.4:50052')
+        self._p4id = 20
+        self._p4agent = {}
+        # self._p4agent[0] = P4Agent('192.168.100.4:50052')
         # self._p4agent[1] = P4Agent('192.168.100.5:50052')
         self.logger.info('P4 controller initialization complete.')
 
@@ -37,39 +47,102 @@ class P4Controller:
             elif msgType == MSG_TYPE_P4CONTROLLER_CMD:
                 self.logger.info('Got a command.')
                 cmd = msg.getbody()
+                print(msg)
                 self._commands[cmd.cmdID] = cmd
-                resDict = {}
+                self._commandresults[cmd.cmdID] = CMD_STATE_PROCESSING
+                resdict = {}
+                success = True
+                '''
                 if cmd.cmdType == CMD_TYPE_ADD_SFC:
-                    pass
+                    success = self._addsfc(cmd)
                 elif cmd.cmdType == CMD_TYPE_DEL_SFC:
-                    pass
+                    success = self._delsfc(cmd)
                 elif cmd.cmdType == CMD_TYPE_ADD_SFCI:
-                    pass
+                    success = self._addsfci(cmd)
                 elif cmd.cmdType == CMD_TYPE_DEL_SFCI:
-                    pass
+                    success = self._delsfci(cmd)
                 elif cmd.cmdType == CMD_TYPE_GET_SFCI_STATE:
-                    pass
+                    success, resdict = self._getstate(cmd)
                 else:
                     self.logger.error("Unsupported cmd type for P4 controller: %s." % cmd.cmdType)
+                if success:
+                    self._commandresults[cmd.cmdID] = CMD_STATE_SUCCESSFUL
+                else:
+                    self._commandresults[cmd.cmdID] = CMD_STATE_FAIL
+                '''
+                cmdreply = CommandReply(cmd.cmdID, self._commandresults[cmd.cmdID])
+                cmdreply.attributes["zone"] = self.zonename
+                cmdreply.attributes.update(resdict)
+                replymessage = SAMMessage(MSG_TYPE_VNF_CONTROLLER_CMD_REPLY, cmdreply)
+                self._messageAgent.sendMsg(MEDIATOR_QUEUE, replymessage)
             else:
                 self.logger.error('Unsupported msg type for P4 controller: %s.' % msg.getMessageType())
     
-    def _addsfc(self):
-        pass
+    def _addsfc(self, _cmd):
+        # send turbonet command
+        return True
     
-    def _addsfci(self):
-        pass
+    def _addsfci(self, _cmd):
+        sfci = _cmd.attributes['sfci']
+        sfciID = sfci.sfciID
+        self.logger.info('Adding sfci %s.' % sfciID)
+        nfseq = sfci.vnfiSequence
+        for nf in nfseq:
+            for nfi in nf:
+                if isinstance(nfi.node, Switch):
+                    p4id = -1
+                    if nfi.node.switchID == P4CONTROLLER_P4_SWITCH_ID_1:
+                        p4id = 0
+                    elif nfi.node.switchID == P4CONTROLLER_P4_SWITCH_ID_2:
+                        p4id = 1
+                    else:
+                        continue
+                    if nfi.vnfType == VNF_TYPE_FW:
+                        self._p4agent[p4id].addIEGress()
+                    elif nfi.vnfType == VNF_TYPE_RATELIMITER:
+                        self._p4agent[p4id].addIEGress()
+                        self._p4agent[p4id].addRateLimiter()
+                    elif nfi.vnfType == VNF_TYPE_MONITOR:
+                        self._p4agent[p4id].addIEGress()
+                        self._p4agent[p4id].addMonitor()
+                    else:
+                        return False
+        return True
     
-    def _getstate(self):
-        pass
+    def _getstate(self, _cmd):
+        return False, {}
     
-    def _delsfc(self):
-        pass
+    def _delsfc(self, _cmd):
+        # send turbonet command
+        return True
     
-    def _delsfci(self):
-        pass
+    def _delsfci(self, _cmd):
+        sfci = _cmd.attributes['sfci']
+        sfciID = sfci.sfciID
+        self.logger.info('Deleting sfci %s.' % sfciID)
+        nfseq = sfci.vnfiSequence
+        for nf in nfseq:
+            for nfi in nf:
+                if isinstance(nfi.node, Switch):
+                    p4id = -1
+                    if nfi.node.switchID == P4CONTROLLER_P4_SWITCH_ID_1:
+                        p4id = 0
+                    elif nfi.node.switchID == P4CONTROLLER_P4_SWITCH_ID_2:
+                        p4id = 1
+                    else:
+                        continue
+                    if nfi.vnfType == VNF_TYPE_FW:
+                        self._p4agent[p4id].removeIEGress()
+                    elif nfi.vnfType == VNF_TYPE_RATELIMITER:
+                        self._p4agent[p4id].removeIEGress()
+                        self._p4agent[p4id].removeRateLimiter()
+                    elif nfi.vnfType == VNF_TYPE_MONITOR:
+                        self._p4agent[p4id].removeIEGress()
+                        self._p4agent[p4id].removeMonitor()
+                    else:
+                        return False
+        return True
 
 if __name__ == '__main__':
-    zonename = 'p4controller'
-    p4ctl = P4Controller(zonename)
+    p4ctl = P4Controller(TURBONET_ZONE)
     p4ctl.run()
